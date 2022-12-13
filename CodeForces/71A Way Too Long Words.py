df7d4d19111f87e80a9980d9d858997b5f90f863
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2022-12-13 05:02:26 +0200

    Add files via upload

---
+++ CodeForces/71A Way Too Long Words.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n = int(input()) 
+i = 1 
+while(i <= n): 
+    word = str(input()) 
+    if len(word) > 10: 
+        print(f"{word[0]}{len(word) - 2}{word[-1]}") 
+    else: 
+        print(word) 
+    i+=1
+
+
+# In[ ]:
+
+
+
+
