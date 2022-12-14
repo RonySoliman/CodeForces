4b91a9842b735dd8f9f2dd62b0f791defc113dbd
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2022-12-14 05:02:33 +0200

    Add files via upload

---
+++ CodeForces/158A Next Round.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n, k = map(int, input().split())
+a = list(map(int,input().split()))
+
+count = 0
+
+for i in range(n):
+    if (a[i] >= a[k-1]) and (a[i]>0):
+        count+=1
+        
+print(count)
+
