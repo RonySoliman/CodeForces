fa2943b5decfec4785afe7da240d5f4200818395
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2022-12-14 02:28:43 +0200

    Update 50A Domino piling.py

--- CodeForces/50A Domino piling.py
+++ CodeForces/50A Domino piling.py
@@ -5,6 +5,7 @@
 
 
 m, n = map(int, input().split())
+# built-in function that allows you to process and transform all the items in an iterable without using an explicit for loop.
 
 count = int(m*n*0.5)
 print(count)
