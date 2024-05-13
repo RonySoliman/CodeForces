f64c5481c821ac717b385f0876e728f4a7a9e8aa
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-05-13 05:36:09 +0300

    Update E. Max.py

--- CodeForces/E. Max.py
+++ CodeForces/E. Max.py
@@ -7,13 +7,11 @@
 # E. Max
 # https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/E
 
-values = int(input())
-checker = list(map(int, input().split()))
+num = int(input())
+numbers = list(map(int, input().split()))
 
-for values in range(1,1000):
-    for i,j in enumerate(checker):
-        num = max(checker)
-    print(num)
+for i in numbers:
+    print(max(numbers))
     break
     
 # https://stackoverflow.com/questions/29811082/how-to-print-out-a-numbered-list-in-python-3
