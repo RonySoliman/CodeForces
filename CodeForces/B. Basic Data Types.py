d90cada9e86b04ee8913ea045b265a89454ffebe
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-05-13 09:10:39 +0300

    Add files via upload

---
+++ CodeForces/B. Basic Data Types.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# B. Basic Data Types
+# https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+values = list(map(str, input().split()))
+
+for i in values:
+    print(i)
+
