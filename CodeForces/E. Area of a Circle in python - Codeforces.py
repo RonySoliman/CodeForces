527c4587a44d2d8807bdd1f2f9487b9fbcd6c65a
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-05-14 07:43:59 +0300

    Add files via upload

---
+++ CodeForces/E. Area of a Circle in python - Codeforces.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# E. Area of a Circle
+# https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+R = float(input()) # 2.00
+fai = 3.141592653
+Area = fai*pow(R,2) # 12.566370612
+print(Area)
+
