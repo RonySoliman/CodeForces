a6ca6db43c82602341cee2598d815b45fe672386
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-05-20 14:20:57 +0300

    Add files via upload

---
+++ CodeForces/I. Welcome for you with Conditions in python - Codeforces.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# I. Welcome for you with Conditions
+# https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+values = list(map(int,input().split()))
+
+if (values[0] >= values[1]):
+    print("Yes")
+else:
+    print("No")
+
