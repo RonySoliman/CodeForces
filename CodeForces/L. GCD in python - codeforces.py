e12d1c0e0514274e87842efea671ac45dd2b5430
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-07-20 02:09:40 +0300

    L. GCD in python - codeforces.py

--- CodeForces/L. GCD in python - codeforces.py
+++ CodeForces/L. GCD in python - codeforces.py
@@ -9,3 +9,7 @@ for i in range(1, min(values[0], values[1]) + 1):
         newlist.append(i)
 
 print(max(newlist))
+
+# Debugging:
+# min function ensures that I only check potential common divisors up to the smaller of the two input values. This is important because a number greater than the smaller of the two numbers cannot be a divisor of both.
+# By adding 1, we ensure that the loop includes the smallest of the two numbers. For example, range(1, 12 + 1) would generate numbers from 1 to 12.
